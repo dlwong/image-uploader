2199c377b91f1160de54b2e80fee6ef146adc1bc
Dorris Wong <pingki@sbcglobal.net>
2019-09-09 21:17:14 -0700

    Implement image preview before upload

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,19 +3,35 @@ import React, {Component} from 'react';
 class App extends Component {
   constructor(props){
     super(props);
-
+    this.state = {
+      file: '',
+      imagePreview:''
+    }
     this.fileUpload = this.fileUpload.bind(this);
   }
 
   fileUpload(e) {
-    console.log(e.target.files[0])
+    e.preventDefault();
+
+    let reader = new FileReader();
+    let file = e.target.files[0];
+
+    reader.onloadend = () => {
+      this.setState({
+        file: file,
+        imagePreview: reader.result
+      });
+    }
+
+    reader.readAsDataURL(file)
   }
 
 
   render () {
     return ( 
       <div>
-        <input type='file' onChange = {this.fileUpload}/>
+        <input type='file' onChange = {(e) => this.fileUpload(e)}/>
+        <img src={this.state.imagePreview} />
       </div>
     )
 
