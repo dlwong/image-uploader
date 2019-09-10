88f5f8da0a21d479f3c554a5983f35cdcca085ff
Dorris Wong <pingki@sbcglobal.net>
2019-09-09 22:13:14 -0700

    Set up server for photo uploads

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,15 @@
 import React, {Component} from 'react';
+import axios from 'axios';
 
 class App extends Component {
   constructor(props){
     super(props);
     this.state = {
-      file: '',
+      file: null,
       imagePreview:''
     }
     this.fileUpload = this.fileUpload.bind(this);
+    this.uploadHandler = this.uploadHandler.bind(this);
   }
 
   fileUpload(e) {
@@ -26,11 +28,26 @@ class App extends Component {
     reader.readAsDataURL(file)
   }
 
+  uploadHandler(e) {
+    e.preventDefault;
+    const data = new FormData();
+    data.append('file', this.state.file)
+
+    axios.post('http://localhost:3000/upload', data)
+          .then (res => {
+             console.log(res)
+            })
+          .catch((error) => {
+            console.log(error);
+          });
+          
+  }
 
   render () {
     return ( 
       <div>
         <input type='file' onChange = {(e) => this.fileUpload(e)}/>
+        <button type="button" onClick={(e) => this.uploadHandler(e)}>Upload</button> 
         <div className="imgPreview">
         <img src={this.state.imagePreview} />
         </div>
---
+++ server/index.js
@@ -0,0 +1,42 @@
+var express = require('express');
+var app = express();
+const path = require('path');
+const morgan = require('morgan');
+var multer = require('multer')
+var cors = require('cors');
+
+app.use(cors())
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.static(path.join(__dirname, '/../client/public')));
+
+var storage = multer.diskStorage({
+      destination: function (req, file, cb) {
+      cb(null, 'uploads')
+    },
+    filename: function (req, file, cb) {
+      cb(null, Date.now() + '-' +file.originalname )
+    }
+})
+
+var upload = multer({ storage: storage }).single('file')
+
+app.post('/upload',function(req, res) {
+     
+    upload(req, res, function (err) {
+           if (err instanceof multer.MulterError) {
+               return res.status(500).json(err)
+           } else if (err) {
+               return res.status(500).json(err)
+           }
+      return res.status(200).send(req.file)
+
+    })
+
+});
+
+app.listen(3000, function() {
+
+    console.log('App running on port 3000');
+
+});
