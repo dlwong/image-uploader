67c173b7a92657cf5ff40e6c13be8a4be454a76a
Dorris Wong <pingki@sbcglobal.net>
2019-09-18 02:00:11 -0700

    Implement error handling for file size and type

--- server/index.js
+++ server/index.js
@@ -13,13 +13,13 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, '/../client/public')));
 app.use('/uploads', express.static('uploads'))
 
-
 let port = process.env.PORT;
 if (port == null || port == "") {
   port = 3000;
 }
 
-var storage = multer.diskStorage({
+// Set The Storage Engine
+const storage = multer.diskStorage({
       destination: function (req, file, cb) {
       cb(null, 'uploads')
     },
@@ -28,15 +28,38 @@ var storage = multer.diskStorage({
     }
 });
 
-var upload = multer({ storage: storage }).single('file');
+// Initiate Upload
+const upload = multer({
+  storage: storage,
+  limits:{fileSize: 1000000},
+  fileFilter: function(req, file, cb){
+    checkFileType(file, cb);
+  }
+}).single('file');
+
+// Check File Type
+function checkFileType(file, cb){
+
+  const filetypes = /jpeg|jpg|png/;
+  // Check ext
+  const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
+  // Check mime
+  const mimetype = filetypes.test(file.mimetype);
+
+  if(mimetype && extname){
+    return cb(null,true);
+  } else {
+    cb('Error: Images Only!');
+  }
+}
 
 app.post('/upload',function(req, res) {
      
     upload(req, res, function (err) {
            if (err instanceof multer.MulterError) {
-               return res.status(500).json(err)
+               return res.status(500).send(err)
            } else if (err) {
-               return res.status(500).json(err)
+               return res.status(500).send(err)
            }
       return res.status(200).send(req.file)
 
@@ -58,4 +81,4 @@ app.get('/update',function(req, res) {
     })
 });
 
-app.listen(port);
+app.listen(port, () => console.log(`Server started on port ${port}`));
