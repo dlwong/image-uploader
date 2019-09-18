dde9ae3c30746572777ad182e8e496c52dac1ba1
Dorris Wong <pingki@sbcglobal.net>
2019-09-18 02:00:42 -0700

    Implement error handling for file size and type on frontend

--- client/src/Upload.jsx
+++ client/src/Upload.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import axios from 'axios';
 import { Link, withRouter } from "react-router-dom";
 
@@ -7,7 +7,8 @@ class Upload extends Component {
     super(props);
     this.state = {
       file: null,
-      imagePreview: ''
+      imagePreview: '',
+      error: ''
     }
     this.fileUpload = this.fileUpload.bind(this);
     this.uploadHandler = this.uploadHandler.bind(this);
@@ -18,36 +19,48 @@ class Upload extends Component {
 
     let reader = new FileReader();
     let file = e.target.files[0];
-    //when reading is done processing then set image state
-    reader.onloadend = () => {
-      this.setState({
-        file: file,
-        imagePreview: reader.result
-      });
+    const fileTypes = ['jpg', 'jpeg', 'png'];
+    
+    if (fileTypes.includes(e.target.files[0].name.split('.').pop().toLowerCase())){
+        //when reading is done processing then set image state
+        reader.onloadend = () => {
+          this.setState({
+            file: file,
+            imagePreview: reader.result
+          });
+        }
+      reader.readAsDataURL(file)
     }
-    reader.readAsDataURL(file)
+    
   }
 
   uploadHandler(e) {
     e.preventDefault;
     //send image to server
-    const data = new FormData();
-    data.append('file', this.state.file)
-    
-    axios.post('/upload', data)
-          .then (res => {
-             console.log(res);
-             //redirect back to homepage
-             this.props.history.push("/"); 
-            })
-          .catch((error) => {
-            console.log(error);
-          });
+    if (this.state.file){
+      const data = new FormData();
+      data.append('file', this.state.file)
+      
+      axios.post('/upload', data)
+            .then (res => {
+               console.log(res);
+               //redirect back to homepage
+               this.props.history.push("/"); 
+              })
+            .catch((error) => {
+              this.setState({error: 'File Too Large'});
+            });
+    }else {
+      this.setState({error: 'Not an Image'});
+    }
   }
 
   render () {
     return ( 
-      <div>
+      <Fragment>
+        <p>{
+          this.state.error 
+        }</p>
         <input type='file' onChange = {e => this.fileUpload(e)}/>
         <button type="button" onClick={e => this.uploadHandler(e)}>Upload</button> 
         <div className="imgPreview">
@@ -55,7 +68,7 @@ class Upload extends Component {
             <img src={this.state.imagePreview} />
           }
         </div>
-      </div>
+      </Fragment>
     )
   }
 }
